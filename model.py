fa36b792119a772c8c0f65f43f94f35a65ec06eb
Arun <arun.jb25@hotmail.co.uk>
2022-11-09 16:26:01 +0000

    custom model created issues #11 complete

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
 import datasets
 from torch.utils.tensorboard import SummaryWriter
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
 import torch
 
 
 def train(model, epochs = 10):
 
-    optimiser = torch.optim.SGD(model.parameters(),lr=0.001)
+    optimiser = torch.optim.SGD(model.parameters(),lr=0.0001)
     writer = SummaryWriter()
     batch_idx = 0
 
@@ -31,7 +32,9 @@ class CNN(torch.nn.Module):
             torch.nn.Conv2d(9,18,8,stride=2),
             torch.nn.ReLU(),
             torch.nn.Flatten(),
-            torch.nn.Linear(272322,13),
+            torch.nn.Linear(272322,500),
+            torch.nn.ReLU(),
+            torch.nn.Linear(500,13),
             torch.nn.ReLU(),
             torch.nn.Softmax(dim=1)  
         )
@@ -43,6 +46,17 @@ class CNN(torch.nn.Module):
 
 if __name__ == '__main__':
     dataset = datasets.img_dataset()
-    train_dataloader = datasets.DataLoader(dataset, batch_size=8, shuffle=True)
+    train_size = int(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+    train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
     model = CNN()
     train(model)
+    torch.save(model,'trained_model#1')
+    test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=True)
+
+    for i, (features,labels) in enumerate(test_dataloader):
+        predictions = model(features)
+        print(predictions, labels)
+        print(dataset.labels_dict)
+        break
