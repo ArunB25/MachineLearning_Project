dd9d457bbbb576ac94dd9ade50224326c7aab09a
Arun <arun.jb25@hotmail.co.uk>
2022-11-20 15:45:27 +0000

    api and requirements created

--- api.py
+++ api.py
@@ -10,38 +10,7 @@ from fastapi import Form
 import torch
 import torch.nn as nn
 from torchvision import transforms
-
-
-class TextClassifier(nn.Module):
-    def __init__(self,
-                 decoder: dict = None):
-        super(TextClassifier, self).__init__()
-        pass
-
-##############################################################
-# TODO                                                       #
-# Populate the __init__ method, so that it contains the same #
-# structure as the model you used to train the text model    #
-##############################################################
-        
-        self.decoder = decoder
-    def forward(self, text):
-        x = self.main(text)
-        return x
-
-    def predict(self, text):
-        with torch.no_grad():
-            x = self.forward(text)
-            return x
-    
-    def predict_proba(self, text):
-        with torch.no_grad():
-            pass
-
-
-    def predict_classes(self, text):
-        with torch.no_grad():
-            pass
+import json
 
 class ImageClassifier(nn.Module):
     def __init__(self) -> None:
@@ -54,7 +23,6 @@ class ImageClassifier(nn.Module):
             torch.nn.Linear(fc_inputs, 13),
             torch.nn.ReLU(),
             torch.nn.Softmax(dim=1)) 
-
 
     def forward(self, image):
         preprocess = transforms.Compose([
@@ -74,17 +42,13 @@ class ImageClassifier(nn.Module):
         with torch.no_grad():
             output = self.model(input_batch) # Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
 
-        # probabilities = torch.nn.functional.softmax(output[0], dim=0)# The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+        output = output.squeeze()
+        print(output)
+        category_probs = {}
+        for idx, probability in enumerate(output):
+            category_probs.update({f"{categories[str(idx)]}":round(probability.item(),4)})
 
-        # # Show top categories per image
-        # top5_prob, top5_catid = torch.topk(probabilities, 5)
-        # top_categories = {}
-        # for i in range(top5_prob.size(0)):
-        #     top_category = self.categories[top5_catid[i]]
-        #     top_prob = round(top5_prob[i].item(),4)
-        #     top_categories[top_category] = top_prob
-
-        return output
+        return category_probs
 
 
     def predict(self, image):
@@ -101,53 +65,6 @@ class ImageClassifier(nn.Module):
             pass
 
 
-class CombinedModel(nn.Module):
-    def __init__(self,
-                 decoder: list = None):
-        super(CombinedModel, self).__init__()
-##############################################################
-# TODO                                                       #
-# Populate the __init__ method, so that it contains the same #
-# structure as the model you used to train the combined model#
-##############################################################
-        
-        self.decoder = decoder
-
-    def forward(self, image_features, text_features):
-        pass
-
-    def predict(self, image_features, text_features):
-        with torch.no_grad():
-            combined_features = self.forward(image_features, text_features)
-            return combined_features
-    
-    def predict_proba(self, image_features, text_features):
-        with torch.no_grad():
-            pass
-
-    def predict_classes(self, image_features, text_features):
-        with torch.no_grad():
-            pass
-
-
-# Don't change this, it will be useful for one of the methods in the API
-class TextItem(BaseModel):
-    text: str
-
-
-try:
-##############################################################
-# TODO                                                       #
-# Load the text model. Initialize a class that inherits from #
-# nn.Module, and has the same structure as the text model    #
-# you used for training it, and then load the weights in it. #
-# Also, load the decoder dictionary that you saved as        #
-# text_decoder.pkl                                           #
-##############################################################
-    pass
-except:
-    raise OSError("No Text model found. Check that you have the decoder and the model in the correct location")
-
 try:
     Image_model  = ImageClassifier()
     state_dict = torch.load("model_epoch79.pt")
@@ -158,41 +75,11 @@ except:
     raise OSError("No Image model found. Check that you have the encoder and the model in the correct location")
 
 try:
-##############################################################
-# TODO                                                       #
-# Load the text model. Initialize a class that inherits from #
-# nn.Module, and has the same structure as the combined model#
-# you used for training it, and then load the weights in it. #
-# Also, load the decoder dictionary that you saved as        #
-# combined_decoder.pkl                                       #
-##############################################################
+    with open('categories_dict.json', 'r') as file:
+        categories = json.load(file)
     pass
 except:
-    raise OSError("No Combined model found. Check that you have the encoder and the model in the correct location")
-
-try:
-##############################################################
-# TODO                                                       #
-# Initialize the text processor that you will use to process #
-# the text that you users will send to your API.             #
-# Make sure that the max_length you use is the same you used #
-# when you trained the model. If you used two different      #
-# lengths for the Text and the Combined model, initialize two#
-# text processors, one for each model                        #
-##############################################################
-    pass
-except:
-    raise OSError("No Text processor found. Check that you have the encoder and the model in the correct location")
-
-try:
-##############################################################
-# TODO                                                       #
-# Initialize the image processor that you will use to process#
-# the text that you users will send to your API              #
-##############################################################
-    pass
-except:
-    raise OSError("No Image processor found. Check that you have the encoder and the model in the correct location")
+    raise OSError("No categorys found. Check that you have the category file in the correct location")
 
 app = FastAPI()
 print("Starting server")
@@ -203,22 +90,7 @@ def healthcheck():
   
   return {"message": msg}
 
-@app.post('/predict/text')
-def predict_text(text: TextItem):
-  
-    ##############################################################
-    # TODO                                                       #
-    # Process the input and use it as input for the text model   #
-    # text.text is the text that the user sent to your API       #
-    # Apply the corresponding methods to compute the category    #
-    # and the probabilities                                      #
-    ##############################################################
 
-    return JSONResponse(content={
-        "Category": "", # Return the category here
-        "Probabilities": "" # Return a list or dict of probabilities here
-            })
-  
   
 @app.post('/predict/image')
 def predict_image(image: UploadFile = File(...)):
@@ -226,37 +98,10 @@ def predict_image(image: UploadFile = File(...)):
 
     predictions = Image_model(pil_image)
     
-    ##############################################################
-    # TODO                                                       #
-    # Process the input and use it as input for the image model  #
-    # image.file is the image that the user sent to your API     #
-    # Apply the corresponding methods to compute the category    #
-    # and the probabilities                                      #
-    ##############################################################
-
     return JSONResponse(content={
-    "Category": "", # Return the category here
-    "Probabilities": f"{predictions}" # Return a list or dict of probabilities here
-        })
-  
-@app.post('/predict/combined')
-def predict_combined(image: UploadFile = File(...), text: str = Form(...)):
-    print(text)
-    pil_image = Image.open(image.file)
-    
-    ##############################################################
-    # TODO                                                       #
-    # Process the input and use it as input for the image model  #
-    # image.file is the image that the user sent to your API     #
-    # In this case, text is the text that the user sent to your  #
-    # Apply the corresponding methods to compute the category    #
-    # and the probabilities                                      #
-    ##############################################################
-
-    return JSONResponse(content={
-    "Category": "", # Return the category here
-    "Probabilities": "" # Return a list or dict of probabilities here
-        })
+    "Predicted Category": f"{max(predictions, key=predictions.get)}", # Return the category here
+    "All predictions": f"{predictions}" # Return a list or dict of probabilities here
+    })
     
     
 if __name__ == '__main__':
